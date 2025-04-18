e85b0203fc9231861bbf277917c126cfcd5d8816
S. Tharani Nadeeshayani <150177334+tharaninadee@users.noreply.github.com>
2025-04-18 22:50:22 +0530

    Update teacher-add.test.js

--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -3,15 +3,13 @@ process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
+test('Testing edit teachers', async t => {
+    await t.navigateTo("/");
+    await t.click("#teacher-edit-10003");
 
-test('Testing add teachers', async t => {
-    await t.navigateTo("/dbinitialize");
-
-    await t.navigateTo("/addTeacher");
-    await t.typeText("#teacher-id", "123456");
-    await t.typeText("#teacher-name", "Mohan Perera");
-    await t.typeText("#teacher-age", "45");
-    await t.click("#teacher-add");
+    await t.typeText("#teacher-name", "Changed Teacher Name");
+    await t.typeText("#teacher-age", "99");
+    await t.click("#teacher-edit");
 
     await t.navigateTo("/");
 
@@ -19,5 +17,7 @@ test('Testing add teachers', async t => {
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("Mohan Perera");
+    await t.expect(tdText).contains("Changed Teacher Name");
+
+    await t.click("#teacher-delete-10003");
 });
